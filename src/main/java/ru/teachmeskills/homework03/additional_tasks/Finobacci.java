c62d9e81611b3b3f21a257dea04b2e6c29bfe382
NikitaRudy1992 <rudyairsoft1992@gmail.com>
2023-04-15 15:30:00 +0200

    Homework3_additional_tasks

---
+++ src/main/java/ru/teachmeskills/homework03/additional_tasks/Finobacci.java
@@ -0,0 +1,17 @@
+package ru.teachmeskills.homework03.additional_tasks;
+
+public class Finobacci {
+    public static void main(String[] args) {
+        int num0 = 0;
+        int num1 = 1;
+        int f_num;
+        System.out.print(num0+" "+num1+" ");
+        for(int i = 3; i <= 11; i++){
+            f_num=num0+num1;
+            System.out.print(f_num+" ");
+            num0=num1;
+            num1=f_num;
+        }
+    }
+}
+
