d3fe23e4c53f9c2b2aedea1b1b27700dfa090294
NikitaRudy1992 <rudyairsoft1992@gmail.com>
2023-04-19 13:57:32 +0200

    Homework04.RandomNumbers

---
+++ src/main/java/ru/teachmeskills/homework04/RandomNumbers.java
@@ -0,0 +1,18 @@
+package ru.teachmeskills.homework04;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
