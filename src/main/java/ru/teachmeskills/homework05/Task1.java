8157cca2c724b141518ced051d2f5c7294712d20
NikitaRudy1992 <rudyairsoft1992@gmail.com>
2023-04-25 23:43:42 +0200

    Fix HW 4/5

--- src/main/java/ru/teachmeskills/homework05/Task1.java
+++ src/main/java/ru/teachmeskills/homework05/Task1.java
@@ -7,16 +7,16 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размер массива (строки, столбцы, глубина: ");
-        int size_1 = scanner.nextInt();
-        int size_2 = scanner.nextInt();
-        int size_3 = scanner.nextInt();
+        int size1 = scanner.nextInt();
+        int size2 = scanner.nextInt();
+        int size3 = scanner.nextInt();
         System.out.println("Введите число");
         int number = scanner.nextInt();
         Random random = new Random();
-        int[][][] array = new int[size_1][size_2][size_3];
-        for (int i = 0; i < size_1; i++) {
-            for (int j = 0; j < size_2; j++) {
-                for (int k = 0; k < size_3; k++) {
+        int[][][] array = new int[size1][size2][size3];
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                for (int k = 0; k < size3; k++) {
                     array[i][j][k] = random.nextInt(20);
                     System.out.print(array[i][j][k] + " ");
                 }
@@ -24,9 +24,9 @@ public class Task1 {
             }
             System.out.println();
         }
-        for (int i = 0; i < size_1; i++) {
-            for (int j = 0; j < size_2; j++) {
-                for (int k = 0; k < size_3; k++) {
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                for (int k = 0; k < size3; k++) {
                     array[i][j][k] += number;
                     System.out.print(array[i][j][k] + " ");
                 }
